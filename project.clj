16f613229919b381665afa80586405bf04932e5d
Santiago de Pedro <santiago.pedro23@gmail.com>
2017-05-18 19:39:16 -0400

    issue #2

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/ieer/power"
   :license {:name "Eclipse Public License"
             :url "https://github.com/ieer/power/blob/master/LICENSE"}
+  :repl-options {:timeout 120000}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-gpio "0.2.0"]])
