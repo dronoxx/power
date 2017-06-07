0b847e8d9d7842bf74ea05266e2af35d4bc233e0
Santiago de Pedro <santiago.pedro23@gmail.com>
2017-06-07 19:30:09 -0400

    related to issue #5

--- project.clj
+++ project.clj
@@ -1,7 +1,12 @@
-(defproject power "0.1.0-SNAPSHOT"
+(defproject power "0.1.0"
   :description "Library to power devices in rpi using clojure"
   :url "https://github.com/ieer/power"
   :license {:name "Eclipse Public License"
             :url "https://github.com/ieer/power/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:uberjar {:aot :all}})
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}}
+  :repositories [["releases" {:url           "https://clojars.org/repo"
+                              :username      :env/CLOJAR_USERNAME
+                              :password      :env/CLOJAR_PASSWORD
+                              :sign-releases false}]])
--- src/power/main.clj
+++
@@ -1,9 +0,0 @@
-(ns power.main
-  (:require [power.core :refer :all]
-            [power.py-relay :refer :all])
-  (:gen-class))
-
-(defn -main [& args]
-  (let [device (make-relay-device)]
-    (with-device device
-                 (power (keyword (first args)) 5 :second))))
