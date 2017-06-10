d3ad2aaa93c5dbc98a92756e229502aef3f36ecf
Santiago de Pedro <santiago.pedro23@gmail.com>
2017-06-10 13:44:14 -0400

    related to issue #6

--- project.clj
+++ project.clj
@@ -2,14 +2,9 @@
   :description "Library to power devices in rpi using clojure"
   :url "https://github.com/ieer/power"
   :license {:name "Eclipse Public License"
-            :url "https://github.com/ieer/power/blob/master/LICENSE"}
+            :url  "https://github.com/ieer/power/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :repl-options {
-                 ;; If nREPL takes too long to load it may timeout,
-                 ;; increase this to wait longer before timing out.
-                 ;; Defaults to 30000 (30 seconds)
-                 :timeout 120000
-                 }
+  :repl-options {:timeout 120000}
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
   :repositories [["releases" {:url           "https://clojars.org/repo"
